bc9a42de03b5c37eb8bb9a53e6553ade9ea02c55
Oliver Pope <popeoliv@msu.edu>
2023-08-17 23:34:16 -0400

    more-updates

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -13,7 +13,20 @@
     userName = "Oliver Pope";
     extraConfig = {
       pull.rebase = true;
+      push.autoSetupRemote = true;
       init.defaultBranch = "main";
+      column.ui = "auto";
+      difftool."Kaleidoscope".cmd = "ksdiff --partial-changeset --relative-path \"$MERGED\" -- \"$LOCAL\" \"$REMOTE\"";
+      difftool = {
+        prompt = false;
+        trustExitCode = true;
+      };
+      mergetool."Kaleidoscope" = {
+        cmd = "ksdiff --merge --output \"$MERGED\" --base \"$BASE\" -- \"$LOCAL\" --snapshot \"$REMOTE\" --snapshot";
+        trustExitCode = true;
+      };
+      diff.tool = "Kaleidoscope";
+      feature.manyFiles = true;
     };
     package = pkgs.gitAndTools.gitFull;
     lfs.enable = true;
