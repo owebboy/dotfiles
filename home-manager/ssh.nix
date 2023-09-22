b0a8494f52ff662402a14d3d4800d10641258614
Oliver Pope <popeoliv@msu.edu>
2023-09-22 11:18:30 -0400

    ssh aliases

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -3,4 +3,16 @@
   matchBlocks."*".extraOptions = {
     "IdentityAgent" = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
   };
+  matchBlocks."webld5" = {
+    hostname = "webld5.bus.msu.edu";
+    user = "popeoliv@campusad.msu.edu";
+  };
+  matchBlocks."weblt5" = {
+    hostname = "weblt5.bus.msu.edu";
+    user = "popeoliv@campusad.msu.edu";
+  };
+  matchBlocks."weblp5" = {
+    hostname = "weblp5.bus.msu.edu";
+    user = "popeoliv@campusad.msu.edu";
+  };
 }
