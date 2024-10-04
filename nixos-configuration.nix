4decf76969f6dbe45a7a08b7413141289074ae41
Oliver Pope <popeoliv@msu.edu>
2024-10-04 19:58:33 -0400

    gtk

--- nixos-configuration.nix
+++ nixos-configuration.nix
@@ -40,22 +40,4 @@
 
   system.copySystemConfiguration = true;
   system.stateVersion = "24.05";
-  gtk = {
-    enable = true;
-
-    theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
-    };
-
-    iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-
-    font = {
-      name = "Sans";
-      size = 11;
-    };
-  };
 }
