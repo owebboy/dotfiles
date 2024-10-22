a1711b8d506f4f1adc0a71456b4945a577725575
Oliver Pope <popeoliv@msu.edu>
2024-10-22 11:44:15 -0400

    wezterm flake

--- darwin-nix/nixSettings.nix
+++ darwin-nix/nixSettings.nix
@@ -13,12 +13,14 @@
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
       "https://hydra.nixos.org"
+      "https://wezterm.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+      "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
     ];
     extra-sandbox-paths = [
       "/System/Library/Frameworks"
