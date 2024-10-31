1a79a6004f54b11e55293e0980a1a40ec01afff2
Oliver Pope <opope@me.com>
2024-10-30 23:19:46 -0400

    tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -37,17 +37,7 @@
         '';
       }
 
-      tmuxPlugins.cpu
+      #tmuxPlugins.cpu
     ];
-
-    extraConfig = ''
-      set -g status-right-length 100
-      set -g status-left-length 100
-      set -g status-left ""
-      set -g status-right "#{E:@catppuccin_status_application}"
-      set -agF status-right "#{E:@catppuccin_status_cpu}"
-      set -ag status-right "#{E:@catppuccin_status_session}"
-      set -ag status-right "#{E:@catppuccin_status_uptime}"
-    '';
   };
 }
