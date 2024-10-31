fbcbf5b4dce8f34934696099c5faf4140f22c210
Oliver Pope <opope@me.com>
2024-10-30 22:26:04 -0400

    tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -9,10 +9,9 @@
     keyMode = "emacs";
     sensibleOnTop = true;
     disableConfirmationPrompt = true;
-    shell = "\${pkgs.fish}/bin/fish";
+    shell = "${pkgs.fish}/bin/fish";
     plugins = with pkgs; [
-      tmuxPlugins.cpu
-      #tmuxPlugins.yank
+            #tmuxPlugins.yank
       tmuxPlugins.fpp
       tmuxPlugins.tmux-fzf
       tmuxPlugins.vim-tmux-navigator
@@ -30,34 +29,23 @@
       {
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
-              # Configure the catppuccin plugin
-              set -g @catppuccin_flavor "mocha"
-              set -g @catppuccin_window_status_style "rounded"
-              set -g @catppuccin_window_number_position "right"
-
-              set -g @catppuccin_window_default_fill "number"
-              set -g @catppuccin_window_default_text "#W"
-
-              set -g @catppuccin_window_current_fill "number"
-              set -g @catppuccin_window_current_text "#W"
-
-              set -g @catppuccin_status_left_separator  " "
-              set -g @catppuccin_status_right_separator ""
-              set -g @catppuccin_status_fill "icon"
-              set -g @catppuccin_status_connect_separator "no"
-
-              set -g @catppuccin_directory_text "#{pane_current_path}"
-
-          set -g status-right-length 100
-          set -g status-left-length 100
-          set -g status-left ""
-          set -g status-right "#{E:@catppuccin_status_application}"
-          set -agF status-right "#{E:@catppuccin_status_cpu}"
-          set -ag status-right "#{E:@catppuccin_status_session}"
-          set -ag status-right "#{E:@catppuccin_status_uptime}"
-          set -agF status-right "#{E:@catppuccin_status_battery}"
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
         '';
       }
+
+tmuxPlugins.cpu
+
     ];
+
+    extraConfig = ''
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+    '';
   };
 }
