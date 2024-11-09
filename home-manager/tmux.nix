bd936d9b78fd00307c46920f88352fe28caf7196
Oliver Pope <opope@me.com>
2024-11-08 22:14:12 -0500

    flake update

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -8,7 +8,7 @@
     newSession = true;
     keyMode = "emacs";
     sensibleOnTop = true;
-    shell = "fish";
+    shell = "/etc/profiles/per-user/oliver/bin/fish";
     baseIndex = 1;
     historyLimit = 102400;
     escapeTime = 10;
